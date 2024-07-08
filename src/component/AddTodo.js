7a96e286fb3699ae3ccbe8aafee129404d9a33fc
Mahendra Shahi <smahendrabikram@gmail.com>
2024-07-08 18:45:04 -0400

    implemented all the tasks

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,10 +1,10 @@
+import Axios from "axios";
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
-  // Create a local react state of the this component with both content date property set to nothing.
   constructor() {
     super();
     this.state = {
@@ -13,9 +13,7 @@ class AddTodo extends Component {
       duedate: null
     };
   }
-  // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
-  // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
-  // into the text field.
+
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
@@ -38,6 +36,24 @@ class AddTodo extends Component {
   // in the Home.js file which then adds the input into the list.
   handleSubmit = (event) => {
     event.preventDefault();
+    const jsonObject = {
+      id: this.state.id,
+      task: this.state.content,
+      currentDate: this.state.date,
+      dueDate: this.state.duedate
+   };
+
+    Axios({
+      method: "POST",
+      url: "http://localhost:8080/add/item",
+      data: {jsonObject},
+      headers: {
+        "Content-Type": "application/json"
+      }
+  }).then(res => {
+      console.log(res.data.message);
+  });
+
     if (this.state.content.trim()) {
       this.props.addTodo(this.state);
       this.setState({
